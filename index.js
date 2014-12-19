58c42990a639159ea31291494c16967c7754b26d
oeuillot <olivier.oeuillot@gmail.com>
2014-12-19 10:11:17 +0100

    First version

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
 var exec = require('child_process').exec;
 var parseurl = require('url');
+var fs = require('fs');
 
 var NO_CACHE_CONTROL = "no-cache, private, no-store, must-revalidate, max-stale=0, max-age=1,post-check=0, pre-check=0";
 
-var DEFAULT_PATH = '/omx';
-var TMP_PATH = "/tmp/";
-var commandParameters = [ "-b" ];
-var MOVIES_PATH = "/home/olivier/Films/";
-
-function omx(configuration, mapper) {
-	this.configuration = configuration;
-	this._map = mapper;
-	return omx.express;
-}
+var OMX_PATH = '/omx';
+var TMP_PATH = "/tmp";
+var COMMAND_PARAMETERS = [ "-b" ];
+var MOVIES_PATH = "/home/olivier/Films";
 
 // 192.168.0.128:8080/omx/start/Barbapapa/Barbapapa+-+Le+facteur.mkv
 
-omx.express = function(req, res, next) {
-	if (req.path.indexOf(DEFAULT_PATH) === 0) {
+function omx(configuration) {
+	configuration = configuration || {};
+
+	this.configuration = configuration;
+
+	this._omxPath = configuration.omxPath || OMX_PATH;
+	this._tmpPath = configuration.tmpPath || TMP_PATH;
+	this._moviesPath = configuration.moviesPath || MOVIES_PATH;
+	this._commandParameters = configuration.commandParameters || COMMAND_PARAMETERS;
+
+	var self = this;
+	this._mapKey('pause', 'p');
+	this._mapKey('quit', 'q', function(callback) {
+		self._release();
+		return callback(null);
+	});
+	this._mapKey('play', '.');
+	this._mapKey('forward', "$'\\x1b\\x5b\\x43'");
+	this._mapKey('backward', "$'\\x1b\\x5b\\x44'");
+
+	this.express = this._express.bind(this);
+}
+
+module.exports = omx;
+
+omx.prototype._express = function(req, res, next) {
+	if (req.path.indexOf(this._omxPath) === 0) {
 		// replace + and decode
+		path = path.substring(this._omxPath.length);
+
 		path = decodeURIComponent(req.path.replace(/\+/g, ' '));
+
 		// remove leading and trailing /
 		path = path.replace(/^\/|\/$/g, '');
 		// split and remove leading path
 		var parts = path.split('/');
-		parts.shift();
 		var command = parts.shift();
-		var path = MOVIES_PATH + parts.join('/');
-		console.log('executing', command, parts);
-		if (omx[command]) {
-			omx[command].call(omx, path, function(error) {
+		var path = this._moviesPath + '/' + parts.join('/');
+		if (this[command]) {
+			console.log('executing', command, parts);
+			this[command].call(this, path, function(error) {
 
 				res.writeHead(200, {
 					'Content-Type': 'application/json',
@@ -45,24 +67,32 @@ omx.express = function(req, res, next) {
 			});
 			return;
 		}
+
+		console.log('Unknown command', command);
+
 	}
 	next();
 };
 
-omx.start = function(moviePathName, callback) {
+omx.prototype.start = function(moviePathName, callback) {
 	if (this._proc) {
 		return callback("Please stop");
 	}
 	var pipe = this._pipe;
 	if (!pipe) {
-		pipe = TMP_PATH + 'omxcontrol-' + (Date.now());
+		pipe = this._tmpPath + '/omxcontrol-' + (Date.now());
 		exec('mkfifo ' + pipe);
 		this._pipe = pipe;
+
+		var self = this;
+		process.on('exit', function(code) {
+			self._release();
+		});
 	}
 
 	var self = this;
 
-	var cmd = 'omxplayer ' + commandParameters.join(" ") + ' "' + moviePathName + '" < ' + pipe;
+	var cmd = 'omxplayer ' + this._commandParameters.join(" ") + ' "' + moviePathName + '" < ' + pipe;
 	console.log("Command=", cmd);
 	var p = exec(cmd, function(error, stdout, stderr) {
 		if (error) {
@@ -92,7 +122,7 @@ omx.start = function(moviePathName, callback) {
 	return callback(null);
 };
 
-omx.sendKey = function(key, callback) {
+omx.prototype.sendKey = function(key, callback) {
 	if (!this._proc) {
 		return callback("No process");
 	}
@@ -105,7 +135,7 @@ omx.sendKey = function(key, callback) {
 	});
 };
 
-omx.mapKey = function(command, key, then) {
+omx.prototype._mapKey = function(command, key, then) {
 	omx[command] = function(path, callback) {
 		omx.sendKey(key, function(error) {
 			if (error) {
@@ -120,15 +150,9 @@ omx.mapKey = function(command, key, then) {
 	};
 };
 
-omx.mapKey('pause', 'p');
-omx.mapKey('quit', 'q', function(callback) {
-	exec('rm ' + this._pipe);
-	this._pipe = null;
-
-	return callback(null);
-});
-omx.mapKey('play', '.');
-omx.mapKey('forward', "$'\\x1b\\x5b\\x43'");
-omx.mapKey('backward', "$'\\x1b\\x5b\\x44'");
-
-module.exports = omx;
+omx.prototype._release = function() {
+	if (this._pipe) {
+		fs.unlink(this._pipe);
+		this._pipe = null;
+	}
+}
